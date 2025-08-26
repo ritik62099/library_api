7650c23403d2c9fbdf39447f11e5053e52792b27
ritik62099 <ritik841207@gmail.com>
2025-08-26 07:19:54 +0530

    Initial commit

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const { markAttendance, getTodayAttendance } = require("../controllers/attendanceController");
-const protectAdmin = require("../middleware/authMiddleware");
+    const express = require("express");
+    const { markAttendance, getTodayAttendance } = require("../controllers/attendanceController");
+    const protectAdmin = require("../middleware/authMiddleware");
 
-const router = express.Router();
+    const router = express.Router();
 
-router.post("/mark", markAttendance); // ✅ Public
-router.get("/today", protectAdmin, getTodayAttendance); // ✅ Admin only
+    router.post("/mark", markAttendance); // ✅ Public
+    router.get("/today", protectAdmin, getTodayAttendance); // ✅ Admin only
 
-module.exports = router;
+    module.exports = router;
